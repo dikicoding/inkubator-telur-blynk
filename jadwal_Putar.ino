cd067fdde59a6547fa59add9b701676bb8544a4a
dikicoding <57633476+dikicoding@users.noreply.github.com>
2022-04-10 02:44:51 +0700

    Update jadwal_Putar.ino
    jadwal putar motor menjadi 3 periode

--- jadwal_Putar.ino
+++ jadwal_Putar.ino
@@ -7,7 +7,6 @@ BLYNK_WRITE(V5) {
   Min_1   = t.getStartMinute();
   Sec_1   = t.getStartSecond();
 }
-
 //Input Time Perputaran ke-2
 BLYNK_WRITE(V6) {
   TimeInputParam t(param);
@@ -15,7 +14,6 @@ BLYNK_WRITE(V6) {
   Min_2   = t.getStartMinute();
   Sec_2   = t.getStartSecond();
 }
-
 //Input Time Perputaran ke-3
 BLYNK_WRITE(V7) {
   TimeInputParam t(param);
@@ -23,38 +21,24 @@ BLYNK_WRITE(V7) {
   Min_3   = t.getStartMinute();
   Sec_3   = t.getStartSecond();
 }
-//Input Time Perputaran ke-4
-BLYNK_WRITE(V8) {
-  TimeInputParam t(param);
-  Hour_4  = t.getStartHour();
-  Min_4   = t.getStartMinute();
-  Sec_4   = t.getStartSecond();
-}
-//------------- WAKTU Perputaran -------------
 
+//------------- WAKTU Perputaran -------------
 void jadwal_Putar() {
   DateTime now = rtc.now();
   sprintf(currentTime, "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
   Blynk.virtualWrite(V9, currentTime);
-//    Serial.print(now.hour(), DEC);
-//    Serial.print(':');
-//    Serial.print(now.minute(), DEC);
-//    Serial.print(':');
-//    Serial.print(now.second(), DEC);
-//    Serial.println();
-  sama_1 = (now.hour() == Hour_1  && now.minute() == Min_1   && now.second() == Sec_1);
-  sama_2 = (now.hour() == Hour_2  && now.minute() == Min_2   && now.second() == Sec_2);
-  sama_3 = (now.hour() == Hour_3  && now.minute() == Min_3   && now.second() == Sec_3);
-  sama_4 = (now.hour() == Hour_4  && now.minute() == Min_4   && now.second() == Sec_4);
+  
+  putar_1 = (now.hour() == Hour_1 && now.minute() == Min_1 && now.second() == Sec_1);
+  putar_2 = (now.hour() == Hour_2 && now.minute() == Min_2 && now.second() == Sec_2);
+  putar_3 = (now.hour() == Hour_3 && now.minute() == Min_3 && now.second() == Sec_3);
 
-  if ( sama_1 || sama_2 || sama_3 || sama_4) {
-    digitalWrite(pin_motor_putar, LOW);             //Relay aktif untuk menyalakan motor Putar
-    indikasi_motor.on();
-    Blynk.notify("Motor Nyala");
+  if ( putar_1 || putar_2 || putar_3) {
+    digitalWrite(pin_motor_putar, LOW);           //Relay aktif untuk menyalakan motor Putar
+    Blynk.notify("Motor Putar Menyala");
+
     timer.setTimeout(waktu_motor_putar, []() {
-      digitalWrite(pin_motor_putar, HIGH);         //Relay mati untuk mematikan motor putar
-      indikasi_motor.off();
-       Blynk.notify("Motor Hidup");
+    digitalWrite(pin_motor_putar, HIGH);         //Relay mati untuk mematikan motor putar
+    Blynk.notify("Motor Putar Mati");
     });
   }
 }
