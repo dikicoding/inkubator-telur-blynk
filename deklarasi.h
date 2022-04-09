c1bf7ec12a91b0e1281922ef3e8326cf1e70b0cb
dikicoding <57633476+dikicoding@users.noreply.github.com>
2022-04-10 02:42:45 +0700

    Update deklarasi.h
    Penambahan Infrared dan lampu pendeteksi

--- deklarasi.h
+++ deklarasi.h
@@ -3,27 +3,31 @@
 int Hour_1, Min_1, Sec_1;
 int Hour_2, Min_2, Sec_2;
 int Hour_3, Min_3, Sec_3;
-int Hour_4, Min_4, Sec_4;
-boolean sama_1, sama_2, sama_3, sama_4;
+boolean putar_1, putar_2, putar_3, state;
+byte mod;
 
 // RTC
 #include "RTClib.h"
-#include <Wire.h>
 RTC_DS3231 rtc;
 char currentTime[9];
 int waktu_motor_putar = 30000;                        // WAKTU PERPUTARAN MOTOR
-#define pin_motor_putar D6                              //PIN motor putar telur
+#define pin_motor_putar D7                              //PIN motor putar telur
 
 // ------------ Pendeteksi Telur --------------
-#define lampu_pendeteksi D6 //Lampu Pendeteksi
-const int ldrPin = A0; //Pin Sensor LDR
+#define lampu_pendeteksi D5 //Lampu pendeteksi
+#define IR D3
+bool ir;
+#define ldrPin A0 //Pin Sensor LDR
 int ldrValue;
 
 //----------------- Sensor Suhu ------------------
+
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#define ONE_WIRE_BUS D4 //PIN Sensor Suhu
-#define lampu D7 //PIN Lampu
+#include <Wire.h>
+#define ONE_WIRE_BUS D4 //PIN Sensor Suhu DS18B20
+#define lampu D8 //PIN Lampu Inkubator
+#define kipas D6 //PIN kipas Inkubator
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature DS18B20(&oneWire);
 float suhu;
