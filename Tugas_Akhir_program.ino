545533452b4fd4deeff3502ab2e5a241ab71f7d2
dikicoding <57633476+dikicoding@users.noreply.github.com>
2022-04-10 02:41:03 +0700

    Update Tugas_Akhir_program.ino

--- Tugas_Akhir_program.ino
+++ Tugas_Akhir_program.ino
@@ -6,11 +6,14 @@
 BlynkTimer timer;
 WidgetLCD lcd(V0);
 WidgetLED indikasi_lampu(V1);
-WidgetLED indikasi_motor(V2);
+WidgetLED indikasi_lampu_pendeteksi(V10);
+WidgetLED waktu_1(V11);
+WidgetLED waktu_2(V12);
+WidgetLED waktu_3(V13);
 
 char auth[] = "9kd5_os-WUnT4ZzAaNidvvkD3soLIBHt";   // Tokennya di email sesuai akun Blynk
-char ssid[] = "CALISTA";  // SSID internet
-char pass[] = "Mantaptenan"; // pass Wifi 
+char ssid[] = "tugas akhir";  // SSID internet
+char pass[] = "1461700035"; // pass Wifi 
 
 BLYNK_CONNECTED() {
   Blynk.syncAll();
@@ -19,39 +22,38 @@ BLYNK_CONNECTED() {
 void setup(){
 Serial.begin(115200);
 Blynk.begin(auth, ssid, pass, "iot.serangkota.go.id", 8080);
+ 
+//-------Penjadwalan Suhu Inkubator --------------
+timer.setInterval(800L, penjadwalan_suhu); 
+float suhu = DS18B20.getTempCByIndex(0);
+float kelembaban = DS18B20.toFahrenheit(suhu); 
+pinMode(lampu, OUTPUT);
+pinMode(kipas, OUTPUT);
+digitalWrite(kipas, HIGH);
+digitalWrite(kipas, LOW);
+digitalWrite(lampu, HIGH);
+digitalWrite(lampu, LOW);
 
 //------------- Waktu Perputaran ------------
 timer.setInterval(800L, jadwal_Putar);
 pinMode(pin_motor_putar, OUTPUT);
 digitalWrite(pin_motor_putar, HIGH);
 rtc.begin();
-indikasi_motor.off();
-Wire.begin(5, 4);   //Setting wire (5 untuk SDA dan 4 untuk SCL)
-//rtc.adjust(DateTime(2021, 10, 20, 16, 15, 00));
+//rtc.adjust(DateTime(2021, 12, 30, 23, 34, 00));
 //rtc.adjust(DateTime(__DATE__, __TIME__));
 
-
-//------------- Suhu Inkubator --------------
-timer.setInterval(1000L, Sensor_Suhu); 
-pinMode(lampu, OUTPUT);
-digitalWrite(lampu, HIGH);
-digitalWrite(lampu, LOW);
-
 //------------- Pendeteksi Telur -------------
-timer.setInterval(1000L, pendeteksi_telur);  
+timer.setInterval(800L, pendeteksi_telur); 
+pinMode(lampu_pendeteksi, OUTPUT);
+pinMode(IR, INPUT);
+digitalWrite(lampu_pendeteksi, HIGH);
+digitalWrite(lampu_pendeteksi, LOW);
+
+timer.setInterval(1000L, ldr_telur); 
 }
 
 void loop(){
   timer.run();
   Blynk.run();
-  float suhu = DS18B20.getTempCByIndex(0);
-  float kelembaban = DS18B20.toFahrenheit(suhu); 
-  if(suhu < 37.50){                   //Jika suhu < 37.00*C lampu ON
-    indikasi_lampu.on();
-    digitalWrite(lampu, LOW);
-    }
-  else if(suhu > 38.70){              //Jika suhu < 38.50*C lampu OFF
-    indikasi_lampu.off();
-    digitalWrite(lampu, HIGH);
-   }
+  delay(100);
 }
